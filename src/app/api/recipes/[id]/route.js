df6861f4b00ea9644c694020c20162fe4306f992
mb-tousif <meta.block.tousif@gmail.com>
2024-01-17 01:48:10 +0600

    API completed

---
+++ src/app/api/recipes/[id]/route.js
@@ -0,0 +1,101 @@
+import prisma from "@/libs/PrismaClientProvider";
+
+// update recipe
+export async function PATCH(req, { params }) {
+  const updateData = await req.json();
+
+  try {
+    const recipe = await prisma.recipe.update({
+      where: { id: params.id },
+      data: updateData,
+      select: {
+        id: true,
+        title: true,
+        instructions: true,
+        image: true,
+        ingredient: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+
+    return new Response(
+      JSON.stringify({
+        success: true,
+        message: "Recipe updated successfully",
+        recipe,
+      }),
+      {
+        headers: { "Content-Type": "application/json" },
+        status: 200,
+      }
+    );
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        message: error.message,
+        success: false,
+      }),
+      {
+        headers: { "Content-Type": "application/json" },
+        status: 400,
+      }
+    );
+  }
+}
+
+// delete recipe
+export async function DELETE(req, { params }) {
+  try {
+    const isExist = await prisma.recipe.findUnique({
+        where: { id: params.id },
+        });
+    if (!isExist) {
+      return new Response(
+        JSON.stringify({
+          message: "Recipe does not exist",
+          success: false,
+        }),
+        {
+          headers: { "Content-Type": "application/json" },
+          status: 400,
+        }
+      );
+    }
+    const recipe = await prisma.recipe.delete({
+      where: { id: params.id },
+      select: {
+        id: true,
+        title: true,
+        instructions: true,
+        image: true,
+        ingredient: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+
+    return new Response(
+      JSON.stringify({
+        success: true,
+        message: "Recipe deleted successfully",
+        recipe,
+      }),
+      {
+        headers: { "Content-Type": "application/json" },
+        status: 200,
+      }
+    );
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        message: error.message,
+        success: false,
+      }),
+      {
+        headers: { "Content-Type": "application/json" },
+        status: 400,
+      }
+    );
+  }
+}
